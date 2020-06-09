265a4bdd47770f2cdd819bc840edbe331838a2e1
cristian <dumitriucristian@yahoo.com>
2020-06-09 11:54:13 +0300

    first commmit

---
+++ database/seeds/TodoSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todo')->insert([
+            'title' => "Do  something else",
+            'due_date' => Carbon::now()->format('Y-m-d H:i:s'),
+            'done' => 1
+        ]);
+
+        DB::table('todo')->insert([
+            'title' => "Do something",
+            'due_date' => Carbon::now()->format('Y-m-d H:i:s'),
+            'done' => 0
+        ]);
+
+        DB::table('todo')->insert([
+            'title' => "Do nothing",
+            'due_date' => Carbon::now()->format('Y-m-d H:i:s'),
+            'done' => 0
+        ]);
+    }
+}
